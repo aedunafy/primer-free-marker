a089e7eeee08f7621ba4376d9cfe92178c432ab7
JAnand <Joseph_Anand@spe.sony.com>
2019-09-02 23:18:16 +0530

    third commit

---
+++ lrnFreemarker00/src/main/java/com/aedunafy/lrnfm/LearnFreeMarker00.java
@@ -0,0 +1,11 @@
+package com.aedunafy.lrnfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFreeMarker00 {
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFreeMarker00.class, args);
+	}
+}
